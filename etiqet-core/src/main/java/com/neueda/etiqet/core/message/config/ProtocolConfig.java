96de2db262e9dca5447d4caa8c171a880b9fb7f8
David Torelli Rosendo <david.torelli@bitsrain.com>
2019-01-21 08:51:53 +0100

    Partial fix for configuration on optional protocol dictionary

--- etiqet-core/src/main/java/com/neueda/etiqet/core/message/config/ProtocolConfig.java
+++ etiqet-core/src/main/java/com/neueda/etiqet/core/message/config/ProtocolConfig.java
@@ -2,57 +2,47 @@ package com.neueda.etiqet.core.message.config;
 
 import com.neueda.etiqet.core.common.exceptions.EtiqetException;
 import com.neueda.etiqet.core.common.exceptions.UnknownTagException;
-import com.neueda.etiqet.core.config.dtos.Client;
-import com.neueda.etiqet.core.config.dtos.Delegate;
-import com.neueda.etiqet.core.config.dtos.Dictionary;
-import com.neueda.etiqet.core.config.dtos.Message;
-import com.neueda.etiqet.core.config.dtos.Protocol;
-import com.neueda.etiqet.core.config.dtos.StopEvent;
-import com.neueda.etiqet.core.config.dtos.UrlExtension;
+import com.neueda.etiqet.core.config.dtos.*;
 import com.neueda.etiqet.core.config.xml.XmlParser;
 import com.neueda.etiqet.core.util.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ProtocolConfig implements Serializable {
+	
+	/** serialVersionUID */
+	private static final long serialVersionUID = -3777143195512896407L;
 
-    /**
-     * serialVersionUID
-     */
-    private static final long serialVersionUID = -3777143195512896407L;
+	private static final transient Logger LOG = LogManager.getLogger(ProtocolConfig.class);
+	
+	private Protocol protocol;
+	
+	private AbstractDictionary dictionary;
+	
+	private Map<String, Message> messageMap;
 
-    private static final transient Logger LOG = LoggerFactory.getLogger(ProtocolConfig.class);
+	public ProtocolConfig(Protocol protocol) throws EtiqetException {
+		setProtocol(protocol);
+		commonInit();
+	}
+	
+	public ProtocolConfig(String path) throws EtiqetException {		
+		try {
+			setProtocol(new XmlParser().parse(path, Protocol.class));
+			commonInit();
+		} catch (Exception e) {
+			throw new EtiqetException("Unable to load messages configuration: " + path, e);
+		}
+	}
 
-    private Protocol protocol;
-
-    private AbstractDictionary dictionary;
-
-    private Map<String, Message> messageMap;
-
-    public ProtocolConfig() {
-    }
-
-    public ProtocolConfig(Protocol protocol) throws EtiqetException {
-        setProtocol(protocol);
-        commonInit();
-    }
-
-    public ProtocolConfig(String path) throws EtiqetException {
-        try {
-            setProtocol(new XmlParser().parse(path, Protocol.class));
-            commonInit();
-        } catch (Exception e) {
-            throw new EtiqetException("Unable to load messages configuration: " + path, e);
-        }
-    }
-
-    public String getProtocolName() {
-        return protocol.getName();
-    }
+	public String getProtocolName() {
+		return protocol.getName();
+	}
 
     private void commonInit() throws EtiqetException {
         // Setup the ProtocolConfig based on the Protocol parsed from the XML
@@ -103,68 +93,68 @@ public class ProtocolConfig implements Serializable {
         return msgType;
     }
 
-    public String getNameForTag(Integer t) {
-        return getDictionary() != null ? getDictionary().getNameForTag(t) : null;
-    }
+	public String getNameForTag(Integer t) {
+		return getDictionary() != null ? getDictionary().getNameForTag(t) : null;
+	}
+	
+	public Integer getTagForName(String name) throws UnknownTagException {
+		return getDictionary() != null ? getDictionary().getTagForName(name) : null;
+	}
+	
+	public boolean tagContains(Integer tag) {
+		return getDictionary() != null && getDictionary().tagContains(tag);
+	}
+	
+	public String getMsgName(String msgType) {
+		return getDictionary() != null ? getDictionary().getMsgName(msgType) : null;
+	}
+	
+	public boolean isAdmin(String msgName) {
+		return (getDictionary() != null) && getDictionary().isAdmin(msgName);
+	}
+	
+	public boolean isHeaderField(String fieldName) {		
+		return getDictionary() != null && getDictionary().isHeaderField(fieldName);
+	}
+	
+	public boolean isHeaderField(Integer tag) {
+		return getDictionary() != null && getDictionary().isHeaderField(tag);
+	}
+	
+	@Override
+	public String toString() {
+		return getProtocol().toString();
+	}
 
-    public Integer getTagForName(String name) throws UnknownTagException {
-        return getDictionary() != null ? getDictionary().getTagForName(name) : null;
-    }
+	public Delegates getClientDelegates() {
+		return getProtocol().getClient().getDelegates();
+	}
 
-    public boolean tagContains(Integer tag) {
-        return getDictionary() != null && getDictionary().tagContains(tag);
-    }
+	public StopEvent getStopEvent(){
+		return getProtocol().getClient().getStopEvent();
+	}
 
-    public String getMsgName(String msgType) {
-        return getDictionary() != null ? getDictionary().getMsgName(msgType) : null;
-    }
+	public List<UrlExtension> getClientUrlExtensions(){
+		return getProtocol().getClient().getUrlExtensions();
+	}
 
-    public boolean isAdmin(String msgName) {
-        return (getDictionary() != null) && getDictionary().isAdmin(msgName);
-    }
+	public Client getClient() {
+		return getProtocol().getClient();
+	}
 
-    public boolean isHeaderField(String fieldName) {
-        return getDictionary() != null && getDictionary().isHeaderField(fieldName);
-    }
+	public Protocol getProtocol() {
+		return protocol;
+	}
 
-    public boolean isHeaderField(Integer tag) {
-        return getDictionary() != null && getDictionary().isHeaderField(tag);
-    }
+	public void setProtocol(Protocol protocol) {
+		this.protocol = protocol;
+	}
 
-    @Override
-    public String toString() {
-        return getProtocol().toString();
-    }
+	public AbstractDictionary getDictionary() {
+		return dictionary;
+	}
 
-    public List<Delegate> getClientDelegates() {
-        return getProtocol().getClient().getDelegates();
-    }
-
-    public StopEvent getStopEvent() {
-        return getProtocol().getClient().getStopEvent();
-    }
-
-    public List<UrlExtension> getClientUrlExtensions() {
-        return getProtocol().getClient().getUrlExtensions();
-    }
-
-    public Client getClient() {
-        return getProtocol().getClient();
-    }
-
-    public Protocol getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(Protocol protocol) {
-        this.protocol = protocol;
-    }
-
-    public AbstractDictionary getDictionary() {
-        return dictionary;
-    }
-
-    public void setDictionary(AbstractDictionary dictionary) {
-        this.dictionary = dictionary;
-    }
+	public void setDictionary(AbstractDictionary dictionary) {
+		this.dictionary = dictionary;
+	}
 }
