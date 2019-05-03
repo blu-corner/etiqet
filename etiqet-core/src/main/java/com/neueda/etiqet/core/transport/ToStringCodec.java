0a7a9b9797ef91a93cb875d9e026041b089e5819
David Torelli <david.torelli@neueda.com>
2019-05-03 10:23:51 +0200

    Add default codec and transport

---
+++ etiqet-core/src/main/java/com/neueda/etiqet/core/transport/ToStringCodec.java
@@ -0,0 +1,21 @@
+package com.neueda.etiqet.core.transport;
+
+import com.neueda.etiqet.core.common.exceptions.EtiqetException;
+import com.neueda.etiqet.core.message.cdr.Cdr;
+import com.neueda.etiqet.core.util.ParserUtils;
+
+/**
+ * Default codec to string.
+ */
+public class ToStringCodec implements Codec<Cdr, String> {
+
+    @Override
+    public String encode(Cdr message) throws EtiqetException {
+        return message.toString();
+    }
+
+    @Override
+    public Cdr decode(String message) throws EtiqetException {
+        return ParserUtils.stringToCdr("None", message);
+    }
+}
