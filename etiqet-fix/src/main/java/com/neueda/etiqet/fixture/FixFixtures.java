823f631746c1b8504cda25df7807314318fad6ce
Matt License <matt.license@neueda.com>
2019-05-07 11:08:48 +0100

    Adding filtering of groups

--- etiqet-fix/src/main/java/com/neueda/etiqet/fixture/FixFixtures.java
+++ etiqet-fix/src/main/java/com/neueda/etiqet/fixture/FixFixtures.java
@@ -3,16 +3,14 @@ package com.neueda.etiqet.fixture;
 import com.neueda.etiqet.core.message.cdr.Cdr;
 import com.neueda.etiqet.core.message.cdr.CdrItem;
 import com.neueda.etiqet.core.util.ParserUtils;
+import com.neueda.etiqet.core.util.StringUtils;
 import cucumber.api.java.en.Then;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * Class that contains Etiqet Fixtures specific to the FIX protocol.
@@ -123,14 +121,14 @@ public class FixFixtures {
      * Gets a group from the message specified
      *
      * @param message     Message to get the group from
-     * @param messageName Name of the message
+     * @param messageName Name of the message, for logging purposes only
      * @param groupName   Name of the group to get
      * @return {@link CdrItem} that represents the group
      * @throws AssertionError when the group doesn't exist within the message, or the {@code CdrItem} found does not
      *                        have the type {@code CDR_ARRAY}
      */
     CdrItem getGroup(Cdr message, String messageName, String groupName) {
-        if(groupName.contains("/")) {
+        if (groupName.contains("/")) {
             String[] groupSplit = groupName.split("/");
             String firstGroup = groupSplit[0];
             String[] remainingGroups = Arrays.copyOfRange(groupSplit, 1, groupSplit.length);
@@ -145,25 +143,24 @@ public class FixFixtures {
     }
 
     /**
-     *
      * @param message
      * @param messageName
      * @param groupName
      * @param remainingGroups
      * @return
      */
-    CdrItem getNestedGroup(Cdr message, String messageName, String groupName, String[] remainingGroups) {
-        CdrItem group = message.getItem(groupName);
+    private CdrItem getNestedGroup(Cdr message, String messageName, String groupName, String[] remainingGroups) {
+        CdrItem group = getGroupWithFilter(message, groupName);
         assertNotNull("Could not find group " + groupName + " in message " + messageName, group);
         assertEquals("Requested " + groupName + " doesn't appear to be a group, was " + group.getType(),
                      CdrItem.CdrItemType.CDR_ARRAY, group.getType());
 
-        if(remainingGroups.length > 0) {
+        if (remainingGroups.length > 0) {
             String nextGroup = remainingGroups[0];
             String[] groups = Arrays.copyOfRange(remainingGroups, 1, remainingGroups.length);
             Cdr nextMessage = group.getCdrs()
                                    .stream()
-                                   .filter(cdr -> cdr.containsKey(nextGroup))
+                                   .filter(cdr -> cdr.containsKey(getUnfilteredGroupName(nextGroup)))
                                    .findFirst()
                                    .orElse(null);
             assertNotNull("Could not find group " + nextGroup + " in group " + groupName, nextMessage);
@@ -173,13 +170,48 @@ public class FixFixtures {
         return group;
     }
 
-    private void getGroupWithFilter(Cdr message, String groupName) {
-        if(!groupName.contains("[") && !groupName.contains("]")) {
-            return;
+    /**
+     * Returns a {@link CdrItem} matching the group name
+     *
+     * @param message   {@link Cdr} message that the group should belong to
+     * @param groupName name of the group to get from the message. If using a filter should be defined as
+     *                  <code>group[field=name]</code>
+     * @return Matching CdrItem for the group
+     */
+    CdrItem getGroupWithFilter(Cdr message, String groupName) {
+        if (!groupName.contains("[") && !groupName.contains("]")) {
+            return message.getItem(groupName);
         }
 
-        Pattern pattern = Pattern.compile("(.*)(\\[.*])*");
+        int filterStart = groupName.indexOf("[");
+        int filterEnd = groupName.indexOf("]");
+        assertNotEquals("Could not find start of filter '['", -1, filterStart);
+        assertNotEquals("Could not find end of filter ']'", -1, filterEnd);
+        String name = groupName.substring(0, filterStart);
+        String filter = groupName.substring(filterStart + 1, filterEnd);
 
+        String[] split = filter.split("=");
+        String field = split[0];
+        assertFalse("No field specified in group filter " + groupName, StringUtils.isNullOrEmpty(field));
+
+        assertEquals("No value specified in group filter " + groupName, 2, split.length);
+        String value = split[1];
+        assertTrue("Could not find field " + name + " in message " + message, message.containsKey(name));
+        CdrItem childItem = message.getItem(name);
+        assertEquals("Field " + name + " was not a group", CdrItem.CdrItemType.CDR_ARRAY, childItem.getType());
+        assertTrue("Could not find matching group " + groupName, childItem.getCdrs()
+                                                                          .stream()
+                                                                          .anyMatch(cdr -> cdr.containsKey(field) && cdr.getAsString(field).equals(value)));
+        return childItem;
+    }
+
+    private String getUnfilteredGroupName(String groupName) {
+        if (!groupName.contains("[") && !groupName.contains("]")) {
+            return groupName;
+        }
+        int filterStart = groupName.indexOf("[");
+        assertNotEquals("Could not find start of filter '['", -1, filterStart);
+        return groupName.substring(0, filterStart);
     }
 
 }
--- etiqet-fix/src/test/java/com/neueda/etiqet/fixture/FixFixturesTest.java
+++ etiqet-fix/src/test/java/com/neueda/etiqet/fixture/FixFixturesTest.java
@@ -2,10 +2,12 @@ package com.neueda.etiqet.fixture;
 
 import com.neueda.etiqet.core.message.cdr.Cdr;
 import com.neueda.etiqet.core.message.cdr.CdrItem;
+import edu.emory.mathcs.backport.java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -168,6 +170,184 @@ public class FixFixturesTest {
         } catch (Throwable t) {
             assertTrue(t instanceof AssertionError);
             assertEquals("No message found to add group Parties", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_NoFilterSpecified() {
+        Cdr message = new Cdr("Test");
+        message.set("field", "value");
+
+        CdrItem field = fixtures.getGroupWithFilter(message, "field");
+        CdrItem expected = new CdrItem(CdrItem.CdrItemType.CDR_STRING);
+        expected.setStrval("value");
+        assertEquals(expected, field);
+    }
+
+    @Test
+    public void testGetGroupWithFilter_FilterApplied() {
+        CdrItem field = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr child1 = new Cdr("Test");
+        child1.set("child", "value1");
+        Cdr child2 = new Cdr("Test");
+        child2.set("child", "value2");
+        field.setCdrs(Arrays.asList(child1, child2));
+
+        Cdr parent = new Cdr("Parent");
+        parent.setItem("Test", field);
+
+        CdrItem filteredGroup = fixtures.getGroupWithFilter(parent, "Test[child=value1]");
+        assertEquals(field, filteredGroup);
+
+        filteredGroup = fixtures.getGroupWithFilter(parent, "Test[child=value2]");
+        assertEquals(field, filteredGroup);
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_MissingEnd() {
+        try {
+            fixtures.getGroupWithFilter(new Cdr("Test"), "Test[");
+            fail("Should have failed because there is no closing filter bracket");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Could not find end of filter ']'. Actual: -1", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_MissingStart() {
+        try {
+            fixtures.getGroupWithFilter(new Cdr("Test"), "Test]");
+            fail("Should have failed because there is no opening filter bracket");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Could not find start of filter '['. Actual: -1", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_MissingField() {
+        try {
+            fixtures.getGroupWithFilter(new Cdr("Test"), "Test[=value]");
+            fail("Should have failed because there is no field");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("No field specified in group filter Test[=value]", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_MissingValue() {
+        try {
+            fixtures.getGroupWithFilter(new Cdr("Test"), "Test[field=]");
+            fail("Should have failed because there is no value");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("No value specified in group filter Test[field=] expected:<2> but was:<1>", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_FieldNotFound() {
+        Cdr message = new Cdr("Test");
+        message.set("field", "value");
+        try {
+            fixtures.getGroupWithFilter(message, "otherField[field=value]");
+            fail("Should have failed because otherField wasn't defined");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Could not find field otherField in message " + message, t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroupWithFilter_InvalidFilter_FieldNotArrayType() {
+        Cdr message = new Cdr("Test");
+        message.set("field", "value");
+        try {
+            fixtures.getGroupWithFilter(message, "field[field=value]");
+            fail("Should have failed because field is a String type");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Field field was not a group expected:<CDR_ARRAY> but was:<CDR_STRING>", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroup_Nested() {
+        CdrItem subfield2 = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr subChild1 = new Cdr("Test");
+        subChild1.set("subChild", "value2");
+        subChild1.set("subChild2", "value3");
+        subfield2.setCdrs(Collections.singletonList(subChild1));
+
+        CdrItem subfield = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr subChild2 = new Cdr("Test");
+        subChild2.set("subChild", "value3");
+        subfield.setCdrs(Collections.singletonList(subChild2));
+
+        CdrItem field = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr child1 = new Cdr("Test");
+        child1.set("child", "value1");
+        child1.setItem("child2", subfield);
+        Cdr child2 = new Cdr("Test");
+        child2.set("child", "value2");
+        child2.setItem("child2", subfield2);
+        field.setCdrs(Arrays.asList(child1, child2));
+
+        Cdr parent = new Cdr("Parent");
+        parent.setItem("Test", field);
+
+        CdrItem group = fixtures.getGroup(parent, "TestMessage", "Test/child2[subChild=value3]");
+        assertNotNull(group);
+    }
+
+    @Test
+    public void testGetGroup_NonNested() {
+        CdrItem field = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr child1 = new Cdr("Test");
+        child1.set("child", "value1");
+        Cdr child2 = new Cdr("Test");
+        child2.set("child", "value2");
+        field.setCdrs(Arrays.asList(child1, child2));
+
+        Cdr parent = new Cdr("Parent");
+        parent.setItem("Test", field);
+
+        CdrItem group = fixtures.getGroup(parent, "TestMessage", "Test");
+        assertEquals(field, group);
+    }
+
+    @Test
+    public void testGetGroup_NonNested_NotFound() {
+        CdrItem field = new CdrItem(CdrItem.CdrItemType.CDR_ARRAY);
+        Cdr child1 = new Cdr("Test");
+        child1.set("child", "value1");
+        Cdr child2 = new Cdr("Test");
+        child2.set("child", "value2");
+        field.setCdrs(Arrays.asList(child1, child2));
+
+        Cdr parent = new Cdr("Parent");
+        parent.setItem("Test", field);
+
+        try {
+            fixtures.getGroup(parent, "TestMessage", "Test2");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Could not find group Test2 in message TestMessage", t.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetGroup_NonNested_NotArrayType() {
+        Cdr parent = new Cdr("Parent");
+        parent.set("Test", "Value");
+        try {
+            fixtures.getGroup(parent, "TestMessage", "Test");
+        } catch (Throwable t) {
+            assertTrue(t instanceof AssertionError);
+            assertEquals("Requested Test doesn't appear to be a group, was CDR_STRING expected:<CDR_ARRAY> but was:<CDR_STRING>",
+                         t.getMessage());
         }
     }
 
