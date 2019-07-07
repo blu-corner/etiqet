da54594f1dcc8bfcb925fbf8ddf17f66ae2827ef
Ryan McCauley <ryanmccauley211@gmail.com>
2019-07-07 18:33:09 +0100

    Add settings class and .cfg to handle overall behaviour

--- etiqet-sql/src/test/java/com/neueda/etiqet/sql/config/DbConfig.java
+++ etiqet-sql/src/test/java/com/neueda/etiqet/sql/config/DbConfig.java
@@ -1,7 +1,7 @@
 package com.neueda.etiqet.sql.config;
 
-import com.neueda.etiqet.sql.fixture.SqlBase;
-import com.neueda.etiqet.sql.fixture.SqlServer;
+import com.neueda.etiqet.sql.fixture.DbConn;
+import com.neueda.etiqet.sql.fixture.DbBase;
 import org.apache.log4j.Logger;
 
 import javax.xml.bind.JAXBContext;
@@ -11,9 +11,9 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-public class Config {
+public class DbConfig {
 
-    private static Logger logger = Logger.getLogger(SqlServer.class);
+    private static Logger logger = Logger.getLogger(DbConn.class);
 
     public static void init() {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
@@ -24,7 +24,7 @@ public class Config {
         try {
             logger.info("Initializing etiqet-sql config...");
             File file = new File(configUrl.toURI());
-            JAXBContext jaxbContext = JAXBContext.newInstance(SqlBase.class);
+            JAXBContext jaxbContext = JAXBContext.newInstance(DbBase.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             jaxbUnmarshaller.unmarshal(file);
         }
@@ -33,7 +33,7 @@ public class Config {
             e.printStackTrace();
         }
         catch (JAXBException e) {
-            logger.error("Failed to unmarshall SqlServer to setup connection configs");
+            logger.error("Failed to unmarshall DbConn to setup connection configs");
             e.printStackTrace();
         }
     }
