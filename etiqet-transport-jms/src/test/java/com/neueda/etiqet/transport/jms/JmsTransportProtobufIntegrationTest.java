d4599e5c0c8ffb8295dcc1216a39dc464dcf2969
David Jeronimo <david.jeronimo@neueda.com>
2019-07-10 08:34:41 +0200

    binary conversion delegate added to jms transport

--- etiqet-transport-jms/src/test/java/com/neueda/etiqet/transport/jms/JmsTransportProtobufIntegrationTest.java
+++ etiqet-transport-jms/src/test/java/com/neueda/etiqet/transport/jms/JmsTransportProtobufIntegrationTest.java
@@ -1,15 +1,19 @@
 package com.neueda.etiqet.transport.jms;
 
-import com.example.tutorial.AddressBookProtos;
 import com.neueda.etiqet.core.client.delegate.SinkClientDelegate;
 import com.neueda.etiqet.core.message.cdr.Cdr;
 import com.neueda.etiqet.core.message.cdr.CdrItem;
+import com.neueda.etiqet.core.message.config.AbstractDictionary;
 import com.neueda.etiqet.core.message.dictionary.ProtobufDictionary;
 import com.neueda.etiqet.core.transport.Codec;
 import com.neueda.etiqet.core.transport.ProtobufCodec;
+import com.neueda.etiqet.core.transport.delegate.BinaryMessageConverterDelegate;
+import com.neueda.etiqet.core.transport.delegate.ProtobufBinaryMessageConverterDelegate;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.junit.EmbeddedActiveMQBroker;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.util.Map;
 import java.util.Optional;
@@ -25,19 +29,6 @@ public class JmsTransportProtobufIntegrationTest {
     @Rule
     public EmbeddedActiveMQBroker broker;
 
-    @Before
-    public void setup() throws Exception {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        connectionFactory.setTrustAllPackages(true);
-        jmsTransport = new JmsTransport();
-        Codec codec = new ProtobufCodec();
-        codec.setDictionary(new ProtobufDictionary("config/dictionary/addressbook.desc"));
-        jmsTransport.setCodec(codec);
-        jmsTransport.setConnectionFactory(connectionFactory);
-        jmsTransport.setDelegate(new SinkClientDelegate());
-        jmsTransport.start();
-    }
-
     @After
     public void tearDown() throws Exception {
         jmsTransport.stop();
@@ -45,6 +36,7 @@ public class JmsTransportProtobufIntegrationTest {
 
     @Test
     public void testSubscribeToTopicAndReceiveMessages() throws Exception {
+        initTransport(null);
         final String topicName = "topicTest01";
         BlockingQueue<Cdr> receivedMessages = new LinkedBlockingQueue<>();
         Cdr cdr = aCdr("Person")
@@ -63,6 +55,46 @@ public class JmsTransportProtobufIntegrationTest {
         assertEquals(34, Math.round(values.get("id").getIntval()));
         assertEquals("aaa@aaa.aaa", values.get("email").getStrval());
 
+    }
+
+    @Test
+    public void testSubscribeToTopicAndReceiveBinaryMessages() throws Exception {
+        initTransport(new ProtobufBinaryMessageConverterDelegate());
+        final String topicName = "topicTest01";
+        BlockingQueue<Cdr> receivedMessages = new LinkedBlockingQueue<>();
+        Cdr cdr = aCdr("Person")
+            .withField("name", "PersonName")
+            .withField("id", 34)
+            .withField("email", "aaa@aaa.aaa")
+            .build();
+
+        jmsTransport.subscribeToTopic(Optional.of(topicName), message -> receivedMessages.add(message));
+        jmsTransport.sendToTopic(cdr, Optional.of(topicName));
+        Thread.sleep(200);
+
+        assertEquals(1, receivedMessages.size());
+        Map<String, CdrItem> values = receivedMessages.take().getItems();
+        assertEquals("PersonName", values.get("name").getStrval());
+        assertEquals(34, Math.round(values.get("id").getIntval()));
+        assertEquals("aaa@aaa.aaa", values.get("email").getStrval());
+    }
+
+    private void initTransport(BinaryMessageConverterDelegate binaryMessageConverterDelegate) throws Exception {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        connectionFactory.setTrustAllPackages(true);
+        AbstractDictionary dictionary = new ProtobufDictionary("config/dictionary/addressbook.desc");
+        jmsTransport = new JmsTransport();
+        jmsTransport.setDictionary(dictionary);
+        Codec codec = new ProtobufCodec();
+        codec.setDictionary(dictionary);
+        jmsTransport.setCodec(codec);
+        jmsTransport.setConnectionFactory(connectionFactory);
+        if (binaryMessageConverterDelegate != null) {
+            binaryMessageConverterDelegate.setDictionary(dictionary);
+        }
+        jmsTransport.setBinaryMessageConverterDelegate(binaryMessageConverterDelegate);
+        jmsTransport.setDelegate(new SinkClientDelegate());
+        jmsTransport.start();
     }
 
 }
