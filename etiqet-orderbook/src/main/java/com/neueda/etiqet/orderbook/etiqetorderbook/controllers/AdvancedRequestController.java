7af880ffe404fe61823579e577c694934f87daec
Enol Cachero Ramirez <enol.cacheroramirez@version1.com>
2022-04-21 11:10:46 +0200

    Key & Field combos bound

--- etiqet-orderbook/src/main/java/com/neueda/etiqet/orderbook/etiqetorderbook/controllers/AdvancedRequestController.java
+++ etiqet-orderbook/src/main/java/com/neueda/etiqet/orderbook/etiqetorderbook/controllers/AdvancedRequestController.java
@@ -10,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
@@ -31,15 +32,16 @@ import java.util.stream.Collectors;
 
 public class AdvancedRequestController implements Initializable {
 
-    public ComboBox<String> comboTags;
+    public ComboBox<String> comboFieldTags;
     public TableView<Tag> tableViewTags;
     public TableColumn<Tag, String> tableColumnKey;
     public TableColumn<Tag, String> tableColumnField;
-    public TableColumn<Tag, String>  tableColumnValue;
+    public TableColumn<Tag, String> tableColumnValue;
 
     private final Logger logger = LoggerFactory.getLogger(AdvancedRequestController.class);
     public TextArea textAreaFix;
     public TextField arTextFieldValue;
+    public ComboBox<String> comboKeyTags;
 
     private MainController mainController;
 
@@ -56,14 +58,13 @@ public class AdvancedRequestController implements Initializable {
         tableColumnField.setCellValueFactory(new PropertyValueFactory<>("Field"));
         tableColumnValue.setCellValueFactory(new PropertyValueFactory<>("Value"));
         tableColumnValue.setCellFactory(TextFieldTableCell.forTableColumn());
-        tableColumnValue.setOnEditCommit( data -> {
-            this.logger.info("Edit request: old value -> {}" , data.getOldValue());
-            this.logger.info("Edit request: new value -> {}" , data.getNewValue());
+        tableColumnValue.setOnEditCommit(data -> {
+            this.logger.info("Edit request: old value -> {}", data.getOldValue());
+            this.logger.info("Edit request: new value -> {}", data.getNewValue());
 
-            if (data.getRowValue().getField().equals("SendingTime")){
+            if (data.getRowValue().getField().equals("SendingTime")) {
 
             }
-
             substituteTag(data.getRowValue().getKey(), data.getNewValue());
         });
         tableViewTags.setEditable(true);
@@ -74,47 +75,62 @@ public class AdvancedRequestController implements Initializable {
     }
 
 
-    private void xmlReader(String file){
+    private void xmlReader(String file) {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
-        try{
+        try {
             dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(new File(file));
             doc.getDocumentElement().normalize();
 
             NodeList list = doc.getElementsByTagName("field");
-            for (int i = 0; i < list.getLength(); i++){
+            for (int i = 0; i < list.getLength(); i++) {
                 Node field = list.item(i).getAttributes().item(0);
                 tags.add(field.getTextContent());
                 this.logger.info("Field added: {}", field.getTextContent());
             }
             List<String> sortedTags = tags.stream().sorted().collect(Collectors.toList());
-            comboTags.getItems().addAll(sortedTags);
-            comboTags.getSelectionModel().select(0);
-            new AutoCompleteComboBoxListener<>(comboTags);
-        }catch (Exception ex){
+            sortedTags.forEach(t -> {
+                comboFieldTags.getItems().add(t);
+                comboKeyTags.getItems().add(Utils.getKeyFromValue(t));
+            });
+            comboFieldTags.getSelectionModel().select(0);
+            comboKeyTags.getSelectionModel().select(0);
+            Platform.runLater(() -> {
+                new AutoCompleteComboBoxListener<>(comboFieldTags);
+            });
+
+            Platform.runLater(() -> {
+                new AutoCompleteComboBoxListener<>(comboKeyTags);
+            });
+
+
+        } catch (Exception ex) {
             this.logger.error("Exception in xmlReader: {}", ex.getMessage());
         }
     }
 
     public void arButtonAdd(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
             String text = arTextFieldValue.getText();
-            String selectedItem = comboTags.getSelectionModel().getSelectedItem();
+            String selectedItem = comboFieldTags.getSelectionModel().getSelectedItem();
             Optional<Tag> any = tableViewTags.getItems().stream().filter(t -> t.getField().equals(selectedItem)).findAny();
-            if (any.isEmpty()){
-                if (!StringUtils.isEmpty(text.trim())){
+            if (any.isEmpty()) {
+                if (!StringUtils.isEmpty(text.trim())) {
                     Tag tag = new Tag(Utils.getKeyFromValue(selectedItem), selectedItem, text);
                     tableViewTags.getItems().add(tag);
                     updateFixTextArea();
                 }
-            }else{
+            } else {
                 substituteTag(any.get().getKey(), text);
             }
+        });
+
     }
 
-    public void substituteTag(String tagKey, String newValue){
+    public void substituteTag(String tagKey, String newValue) {
         Optional<Tag> target = tableViewTags.getItems().stream().filter(t -> t.getKey().equals(tagKey)).findFirst();
-        if (target.isPresent()){
+        if (target.isPresent()) {
             Tag modified = target.get();
             modified.setValue(newValue);
             List<Tag> newTagList = new ArrayList<>(tableViewTags.getItems());
@@ -125,21 +141,21 @@ public class AdvancedRequestController implements Initializable {
     }
 
     private void updateFixTextArea() {
-        try{
+        try {
             textAreaFix.setText(Utils.fixEncoder(tableViewTags.getItems()));
-        }catch (Exception ex){
+        } catch (Exception ex) {
             this.logger.warn("Exception updateFixTextArea -> {}", ex.getMessage());
         }
     }
 
     public void arButtonSend(ActionEvent actionEvent) {
         String msgType = tableViewTags.getItems().stream().filter(t -> t.getKey().equals(Constants.KEY_MSG_TYPE)).findFirst().get().getValue();
-        try{
-            switch (msgType){
+        try {
+            switch (msgType) {
                 case "D":
                     NewOrderSingle newOrderSingle = new NewOrderSingle();
-                    for (Tag tag : tableViewTags.getItems()){
-                        newOrderSingle.setField(new StringField(Integer.parseInt(tag.getKey()),tag.getValue()));
+                    for (Tag tag : tableViewTags.getItems()) {
+                        newOrderSingle.setField(new StringField(Integer.parseInt(tag.getKey()), tag.getValue()));
                     }
                     Session.sendToTarget(newOrderSingle);
                     break;
@@ -148,14 +164,14 @@ public class AdvancedRequestController implements Initializable {
                 case "G":
                     break;
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             this.logger.error("Exception in arButtonSend: {}", ex.getMessage());
         }
     }
 
     public void arButtonRemove(ActionEvent actionEvent) {
         Tag selectedItem = tableViewTags.getSelectionModel().getSelectedItem();
-        if (isRemovableItem(selectedItem)){
+        if (isRemovableItem(selectedItem)) {
             Platform.runLater(() -> {
                 tableViewTags.getItems().remove(selectedItem);
                 updateFixTextArea();
@@ -168,7 +184,7 @@ public class AdvancedRequestController implements Initializable {
         Utils.getStage(actionEvent).close();
     }
 
-    private boolean isRemovableItem(Tag tag){
+    private boolean isRemovableItem(Tag tag) {
         if (tag == null) return false;
         boolean isNotBeginString = !tag.getKey().equals(Constants.KEY_BEGIN_STRING);
         boolean isNotBodyLength = !tag.getKey().equals(Constants.KEY_BODY_LENGTH);
@@ -176,12 +192,12 @@ public class AdvancedRequestController implements Initializable {
         return isNotBeginString && isNotBodyLength && isNotMessageType;
     }
 
-    private boolean isAddableItem(Tag tag){
+    private boolean isAddableItem(Tag tag) {
         if (tag == null) return false;
         boolean isNotBeginString = !tag.getKey().equals(Constants.KEY_BEGIN_STRING);
         boolean isNotBodyLength = !tag.getKey().equals(Constants.KEY_BODY_LENGTH);
         boolean isNotMessageType = !tag.getKey().equals(Constants.KEY_MSG_TYPE);
-        boolean isNotChecksum= !tag.getKey().equals(Constants.KEY_CHECKSUM);
+        boolean isNotChecksum = !tag.getKey().equals(Constants.KEY_CHECKSUM);
         return isNotBeginString && isNotBodyLength && isNotMessageType && isNotChecksum;
     }
 
@@ -196,11 +212,11 @@ public class AdvancedRequestController implements Initializable {
         String[] fields = textAreaFix.getText().split("\\|");
         List<String> fieldList = List.of(fields);
         List<Tag> fixToListTags = new ArrayList<>();
-        for (String field : fieldList){
+        for (String field : fieldList) {
             String[] keyValue = field.split("=");
             Tag tag = new Tag();
             tag.setKey(keyValue[0]);
-            if (!NumberUtils.isDigits(tag.getKey())||!Constants.hmTagValue.containsKey(Integer.parseInt(tag.getKey()))){
+            if (!NumberUtils.isDigits(tag.getKey()) || !Constants.hmTagValue.containsKey(Integer.parseInt(tag.getKey()))) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Bad format");
                 alert.setHeaderText(Constants.TAGS_MUST_BE_NUMERIC_AND_VALID_FIX_PROTOCOL_KEYS);
@@ -214,9 +230,9 @@ public class AdvancedRequestController implements Initializable {
             fixToListTags.add(tag);
         }
         arButtonClear(actionEvent);
-        Platform.runLater( () -> {
-            for (Tag tag : fixToListTags){
-                if (isAddableItem(tag)){
+        Platform.runLater(() -> {
+            for (Tag tag : fixToListTags) {
+                if (isAddableItem(tag)) {
                     tableViewTags.getItems().add(tag);
                 }
             }
@@ -224,4 +240,29 @@ public class AdvancedRequestController implements Initializable {
         });
 
     }
+
+
+    public void actionComboFieldTag(ActionEvent actionEvent) {
+        this.logger.info(comboFieldTags.getValue());
+        comboKeyTags.getSelectionModel().select(Utils.getKeyFromValue(comboFieldTags.getValue()));
+    }
+
+    public void actionComboKeysTag(ActionEvent actionEvent) {
+        this.logger.info(comboKeyTags.getValue());
+        comboFieldTags.getSelectionModel().select(Constants.hmTagValue.get(Integer.parseInt(comboKeyTags.getValue())));
+    }
+
+    public void setFocusedAllText(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource().toString().contains("comboKeyTags")) {
+            Platform.runLater(() -> {
+                comboKeyTags.getEditor().selectAll();
+            });
+        } else {
+            Platform.runLater(() -> {
+                comboFieldTags.getEditor().selectAll();
+            });
+        }
+
+    }
+
 }
--- etiqet-orderbook/src/main/java/com/neueda/etiqet/orderbook/etiqetorderbook/utils/AutoCompleteComboBoxListener.java
+++ etiqet-orderbook/src/main/java/com/neueda/etiqet/orderbook/etiqetorderbook/utils/AutoCompleteComboBoxListener.java
@@ -32,50 +32,56 @@ public class AutoCompleteComboBoxListener<T> implements EventHandler<KeyEvent> {
     }
     @Override
     public void handle(KeyEvent event) {
-        if(event.getCode() == KeyCode.UP) {
-            caretPos = -1;
-            moveCaret(comboBox.getEditor().getText().length());
-            return;
-        } else if(event.getCode() == KeyCode.DOWN) {
-            if(!comboBox.isShowing()) {
+        try{
+            if(event.getCode() == KeyCode.UP) {
+                caretPos = -1;
+                moveCaret(comboBox.getEditor().getText().length());
+                return;
+            } else if(event.getCode() == KeyCode.DOWN) {
+                if(!comboBox.isShowing()) {
+                    comboBox.show();
+                }
+                caretPos = -1;
+                moveCaret(comboBox.getEditor().getText().length());
+                return;
+            } else if(event.getCode() == KeyCode.BACK_SPACE) {
+                moveCaretToPos = true;
+                caretPos = comboBox.getEditor().getCaretPosition();
+            } else if(event.getCode() == KeyCode.DELETE) {
+                moveCaretToPos = true;
+                caretPos = comboBox.getEditor().getCaretPosition();
+            }
+
+            if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT
+                || event.isControlDown() || event.getCode() == KeyCode.HOME
+                || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
+                return;
+            }
+
+            ObservableList list = FXCollections.observableArrayList();
+            for (int i=0; i<data.size(); i++) {
+                String value = data.get(i).toString();
+                if(value.toLowerCase().startsWith(
+                    AutoCompleteComboBoxListener.this.comboBox
+                        .getEditor().getText().toLowerCase())) {
+                    list.add(data.get(i));
+                }
+            }
+            String t = comboBox.getEditor().getText();
+
+            comboBox.setItems(list);
+            comboBox.getEditor().setText(t);
+            if(!moveCaretToPos) {
+                caretPos = -1;
+            }
+            moveCaret(t.length());
+            if(!list.isEmpty()) {
                 comboBox.show();
             }
-            caretPos = -1;
-            moveCaret(comboBox.getEditor().getText().length());
-            return;
-        } else if(event.getCode() == KeyCode.BACK_SPACE) {
-            moveCaretToPos = true;
-            caretPos = comboBox.getEditor().getCaretPosition();
-        } else if(event.getCode() == KeyCode.DELETE) {
-            moveCaretToPos = true;
-            caretPos = comboBox.getEditor().getCaretPosition();
+        }catch (Exception ex){
+
         }
 
-        if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT
-            || event.isControlDown() || event.getCode() == KeyCode.HOME
-            || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
-            return;
-        }
-
-        ObservableList list = FXCollections.observableArrayList();
-        for (int i=0; i<data.size(); i++) {
-            if(data.get(i).toString().toLowerCase().startsWith(
-                AutoCompleteComboBoxListener.this.comboBox
-                    .getEditor().getText().toLowerCase())) {
-                list.add(data.get(i));
-            }
-        }
-        String t = comboBox.getEditor().getText();
-
-        comboBox.setItems(list);
-        comboBox.getEditor().setText(t);
-        if(!moveCaretToPos) {
-            caretPos = -1;
-        }
-        moveCaret(t.length());
-        if(!list.isEmpty()) {
-            comboBox.show();
-        }
     }
 
 
